58761cdf43e182714e44bc2ed8275266ebc0c68b
Evan Tucker <eej.tucker@gmail.com>
2018-08-15 16:54:18 -0600

    worked on project cards

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -2,16 +2,13 @@
 $(document).ready(function() {
     console.log('welcome to the console');
 
-    // function myFunction(x) {
-    //     if (x.matches) { // If media query matches
-    //         document.body.style.backgroundColor = "yellow";
-    //     } else {
-    //         document.body.style.backgroundColor = "pink";
-    //     }
-    // }
-    
-    // var x = window.matchMedia("(max-width: 700px)")
-    // myFunction(x) // Call listener function at run time
-    // x.addListener(myFunction) // Attach listener function on state changes
+    $('.project-btn').on('click', function() {
+        if($(this).siblings("img").is(":hidden")) {
+            $(this).siblings().show(1000);
+        } else {
+            $(this).siblings().hide(1000);
+        }
+        
+    });
 
 });
