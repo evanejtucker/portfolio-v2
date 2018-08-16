b69256f2b9b8da7923079ba09478e3be8a73e761
Evan Tucker <eej.tucker@gmail.com>
2018-08-15 18:23:34 -0600

    still working on toggle function

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,14 +1,49 @@
 
 $(document).ready(function() {
     console.log('welcome to the console');
+    
 
-    $('.project-btn').on('click', function() {
-        if($(this).siblings("img").is(":hidden")) {
-            $(this).siblings().show(1000);
+    var toggler = function() {
+        // $('.content').hide();
+
+        $('.project-btn').on('click', function() {
+            // if($(this).siblings().is(":hidden")) {
+            //     $(this).siblings().show();
+            // }
+            // if($(this).siblings().is(":visible")) {
+            //     $(this).siblings().hide();
+            // }
+            if ($(this).siblings().css('display') !== 'none') {
+                console.log('display');
+                $(this).siblings().hide();
+                return;
+            } else {
+                console.log('no dispaly');
+                $(this).siblings().show();
+                return;
+            }
+        });
+    }
+
+    function myFunction(x) {
+        if (x.matches) { // If media query matches
+            $('.project-btn').attr('disabled', false);
+            $('.content').hide();
+            toggler();
         } else {
-            $(this).siblings().hide(1000);
-        }
-        
-    });
+            $('.content').show();
+            $('.project-btn').attr('disabled', true);
+            
 
+        }
+    }
+    
+    var x = window.matchMedia("(max-width: 700px)")
+    myFunction(x) // Call listener function at run time
+    x.addListener(myFunction) // Attach listener function on state changes
+    // opens project detail if closed, and vice versa
+    
+    
+    
+    
 });
